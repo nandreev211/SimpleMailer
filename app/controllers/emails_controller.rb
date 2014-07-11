4d1fe70fe9c983cdff7e01b18e4f412d6931f9a1
Nikolay Andreev <n.andreev.211@gmail.com>
2014-07-12 00:08:28 +0300

    Mailer addition

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,2 +1,36 @@
 class EmailsController < ApplicationController
+  def index
+    @sent=Email.where(is_draft:false).order("created_at desc")
+    @draft=Email.where(is_draft:true).order("created_at desc")
+  end
+
+  def new
+    @email = Email.new
+  end
+
+  def create
+    @email = Email.new(email_params)
+    if @email.save
+      # only send email when it is not saved as draft as email
+      if @email.is_draft!=true
+
+      end
+      redirect_to emails_path #redirect to index action
+    else
+      render new_email_path # if error occurs when saving to db , render with error messages, not redirect
+    end
+  end
+
+  def edit
+    @email = Email.find(params[:id])
+  end
+
+  def update
+
+  end
+
+  private
+    def email_params
+      params.require(:email).permit(:to, :subject, :body, :is_draft)
+    end
 end
---
+++ app/mailers/my_mailer.rb
@@ -0,0 +1,11 @@
+class MyMailer < ActionMailer::Base
+  default from: "n.andreev.211@gmail.com"
+  def new_mail(to,subject,body,attachment)
+    @body = body
+    #in case no attachment , do not send attachment in mail
+    if !attachment.nil?
+      attachments[attachment.original_filename] = attachment.read
+    end
+    mail(to: to, subject: subject)
+  end
+end
