77701f616cdf47f5bed0522e8bdbeb3eb1322a19
Nikolay Andreev <n.andreev.211@gmail.com>
2014-07-12 00:51:16 +0300

    Mailer configuration

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -13,7 +13,7 @@ class EmailsController < ApplicationController
     if @email.save
       # only send email when it is not saved as draft as email
       if @email.is_draft!=true
-
+        MyMailer.new_mail(@email.to,@email.subject,@email.body,params[:attachment]).deliver
       end
       redirect_to emails_path #redirect to index action
     else
@@ -26,7 +26,15 @@ class EmailsController < ApplicationController
   end
 
   def update
-
+    @email = Email.find(params[:id])
+    if @email.update_attributes(email_params)
+      if @email.is_draft!=true
+        MyMailer.new_mail(@email.to,@email.subject,@email.body,params[:attachment]).deliver
+      end
+      redirect_to emails_path
+    else
+      render edit_email_path(@email)
+    end
   end
 
   private
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,16 @@ SimpleMailer::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # these options are only needed if you choose smtp delivery
+  config.action_mailer.smtp_settings = {
+      :address=> "smtp.gmail.com",
+      :port=> 587,
+      :user_name=>'Your Mail Address',
+      :domain=>'example.com',
+      :password=> 'Your Password',
+      :authentication=> :plain,
+      :enable_starttls_auto => true
+  }
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
