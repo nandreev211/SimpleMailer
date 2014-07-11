d66554031ee742ebd62fa0c332806ffcea7efa94
Nikolay Andreev <n.andreev.211@gmail.com>
2014-07-11 22:26:24 +0300

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SimpleMailer::Application.config.secret_key_base = '7801fc452aa51a5027dcd3cf582d69c7bc6f775c50f3220cfecaa53696d1b2c138f46622af25a866b0930cf256cbd7677c2bb34f70dffa99982006ab0415937f'
